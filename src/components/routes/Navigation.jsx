2e5f4f332f884ad54efbf2f88b1b77a00140872d
navarcus <bspalamutcu@gmail.com>
2022-06-26 16:00:24 +0300

    minor changes on route

--- src/components/routes/Navigation.jsx
+++ src/components/routes/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
+      <Route path="/all-products" element={<Shop />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/sale" element={<Sale />} />
