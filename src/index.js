b32002b0994af03f50ef53843031911c2c3db386
navarcus <bspalamutcu@gmail.com>
2022-06-26 16:01:42 +0300

    new store added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import App from './App';
 
