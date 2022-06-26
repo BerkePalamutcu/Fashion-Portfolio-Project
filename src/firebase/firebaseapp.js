3a212904c562752895fabfad36649e16f3bdf815
navarcus <bspalamutcu@gmail.com>
2022-06-26 15:59:58 +0300

    document snapshot returns data

--- src/firebase/firebaseapp.js
+++ src/firebase/firebaseapp.js
@@ -76,12 +76,14 @@ export const getCategoriesAndDocuments = async () => {
   const collectionRef = collection(db, 'categories');
   const q = query(collectionRef);
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
+  // data method comes from firebase
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-  console.log(categoryMap);
-  console.log(querySnapshot);
+  // categories.reduce((acc, category) => {
+  //   const { title, items } = category;
+
+  //   acc[title.toLowerCase()] = items;
+  //   return acc;
+  // }, {});
+  // return categories;
 };
