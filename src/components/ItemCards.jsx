43a86f8c322321b5a7183d6fc979e47f7d7a2924
navarcus <bspalamutcu@gmail.com>
2022-06-29 02:57:42 +0300

    strict mode added to index.js again

--- src/components/ItemCards.jsx
+++ src/components/ItemCards.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDataFromFirestore } from '../redux/dataSlice';
 import { getCategoriesAndDocuments } from '../firebase/firebaseapp';
@@ -10,7 +11,6 @@ const CardsContainer = styled.div`
 `;
 const CardsWrapper = styled.div`
   width: 100vw;
-
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px;
@@ -19,7 +19,7 @@ const CardsWrapper = styled.div`
 const ItemCard = styled.div`
   display: flex;
   font-family: 'Quintessential', cursive;
-  font-weight: 600;
+  font-weight: 500;
   font-size: 20px;
 `;
 const CardImage = styled.img.attrs((props) => ({
@@ -34,10 +34,27 @@ const CardImage = styled.img.attrs((props) => ({
 //COMPONENT
 const ItemCards = () => {
   let itemsData = [];
+  const [intersection, setIntersection] = useState(false);
+  const [fetchedItemCount, setFetchedItemCount] = useState(12);
   const [itemsDataState, setItemsDataState] = useState([]);
+  const bottomElementRef = useRef(null);
 
   const items = useSelector((state) => state.getDataReducer.itemData);
   const dispatch = useDispatch();
+
+  //CALLBACK FUNCTION FOR INTERSECTION OBSERVER
+  const observerHelper = (items) => {
+    let [item] = items;
+    setIntersection(item.isIntersecting);
+  };
+  //OPTIONS FOR OBSERVER
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0,
+  };
+
+  //Redux action to get the data from the store
   useEffect(() => {
     const getData = async () => {
       const shopData = await getCategoriesAndDocuments('categories');
@@ -53,24 +70,55 @@ const ItemCards = () => {
     for (let i = 0; i < Object.values(items).length; i++) {
       for (let j = 0; j < Object.values(items)[i].items.length; j++) {
         itemsData.push(Object.values(items)[i].items[j]);
-        setItemsDataState(itemsData);
+        // setItemsDataState(itemsData);
       }
     }
-  }, []);
+    //TODO:INFINITE SCROLLING WILL BE IMPLEMENTED!
+    //FIXME: Slice logic is handled here for now!
+    let slicedItems = itemsData.slice(0, fetchedItemCount);
+    console.log(slicedItems);
+    setItemsDataState(slicedItems);
+  }, [items]);
+  //
+  let dummyDivRef = bottomElementRef.current;
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerHelper, options);
+
+    if (dummyDivRef) {
+      observer.observe(dummyDivRef);
+      console.log('im here');
+    }
+
+    return () => {
+      if (dummyDivRef) {
+        try {
+          observer.unobserve(dummyDivRef);
+          setFetchedItemCount(fetchedItemCount + 8);
+          console.log(fetchedItemCount);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+  }, [dummyDivRef, options, fetchedItemCount]);
+  //
 
   return (
     <>
       <CardsContainer>
         <CardsWrapper>
           {itemsDataState.map((item) => (
-            <div key={item.id}>
-              <CardImage alt={item.name} src={item.imgURL[0]} />
-              <ItemCard>{item.name}</ItemCard>
-              <div>{item.price}$</div>
-            </div>
+            <>
+              <div key={item.id}>
+                <CardImage alt={item.name} src={item.imgURL[0]} />
+                <ItemCard>{item.name}</ItemCard>
+                <div>{item.price}$</div>
+              </div>
+            </>
           ))}
         </CardsWrapper>
       </CardsContainer>
+      <div ref={bottomElementRef}></div>
     </>
   );
 };
