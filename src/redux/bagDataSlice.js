70b6671c7cde285136521c9d7f112fb1e31823e6
navarcus <bspalamutcu@gmail.com>
2022-07-14 23:06:13 +0300

    new actions added

--- src/redux/bagDataSlice.js
+++ src/redux/bagDataSlice.js
@@ -1,18 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  bagData: {},
+  bagData: [],
 };
 
 export const bagDataSlice = createSlice({
-  name: 'bagDataProducts',
+  name: "bagDataProducts",
   initialState,
   reducers: {
     sendDataToCardComponent: (state, action) => {
-      state.bagData = { ...action.payload };
+      state.bagData.push({ ...action.payload, quantity: 1 });
+      // state.quantityData = { ...state.bagData, ...action.payload };
+    },
+    increaseItemQuantity: (state, { payload }) => {
+      state.bagData.filter((item, i) =>
+        item.id === payload ? (state.bagData[i].quantity += 1) : item
+      );
+    },
+    decreaseItemQuantity: (state, { payload }) => {
+      state.bagData.filter((item, i) =>
+        item.id === payload && item.quantity > 1
+          ? (state.bagData[i].quantity -= 1)
+          : item
+      );
+    },
+    removeItem: (state, { payload }) => {
+      state.bagData = state.bagData.filter((item) => item.id !== payload);
     },
   },
 });
 
-export const { sendDataToCardComponent } = bagDataSlice.actions;
+export const {
+  sendDataToCardComponent,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  removeItem,
+} = bagDataSlice.actions;
 export default bagDataSlice.reducer;
