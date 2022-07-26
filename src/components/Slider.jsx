04d726743bf21a16b91753eb14782c43ea84c9ad
navarcus <bspalamutcu@gmail.com>
2022-07-26 03:02:41 +0300

    Slider component renewed completely

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getDataFromFirestore } from '../redux/dataSlice';
-import styled from 'styled-components';
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
-import { getCategoriesAndDocuments } from '../firebase/firebaseapp';
-
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getDataFromFirestore } from "../redux/dataSlice";
+import styled from "styled-components";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { getCategoriesAndDocuments } from "../firebase/firebaseapp";
+import SliderCard from "./SliderCard";
 //STYLES
 const SliderContainer = styled.div`
-  height: 700px;
+  height: 100vh;
   width: 100vw;
-  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const SliderHeader = styled.h1`
-  font-family: 'Baskervville', serif;
+  font-family: "Baskervville", serif;
   font-size: 56px;
   text-align: center;
   font-weight: 400;
@@ -22,25 +25,25 @@ const SliderHeader = styled.h1`
 
 const ImgContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   gap: 40px;
+  align-items: center;
 `;
 const ArrowContainer = styled.div`
   border: none;
+  width: 70px;
+  height: 70px;
   background-color: #f6f3f4;
-  padding: 20px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
 `;
-const SliderImg = styled.img.attrs((props) => ({
-  src: props.src,
-}))`
-  height: 400px;
-  width: 275px;
+const SliderDiv = styled.div`
+  width: 1200px;
+  display: flex;
+  overflow: hidden;
+
 `;
 //COMPONENT
 const Slider = () => {
@@ -48,7 +51,7 @@ const Slider = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getData = async () => {
-      const shopData = await getCategoriesAndDocuments('categories');
+      const shopData = await getCategoriesAndDocuments("categories");
       dispatch(getDataFromFirestore(shopData));
     };
     getData();
@@ -63,10 +66,9 @@ const Slider = () => {
           <ArrowContainer>
             <ArrowBackIos />
           </ArrowContainer>
-          <SliderImg src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Bous.700S22_snow-white_2.jpg?v=1644239150" />
-          <SliderImg src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Bous.700S22_Chereful_02.jpg?v=1644852421" />
-          <SliderImg src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Mitsi.700S22T_camel_02.jpg?v=1647457502" />
-          <SliderImg src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Zada.700S22_twill_02.jpg?v=1645609377" />
+          <SliderDiv>
+            <SliderCard items={items} />
+          </SliderDiv>
           <ArrowContainer>
             <ArrowForwardIos />
           </ArrowContainer>
